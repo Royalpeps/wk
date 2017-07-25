68ec306863803780f9b4b99c3d2b8f5ba689b5e4
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-25 10:41:33 +0200

    Added relations between models

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,4 +1,9 @@
 class Kid < ApplicationRecord
   belongs_to :user
-  has_many :reservations, through: :users
+  has_one :reservation, through: :user
+  has_one :weekly_schedule, through: :reservation
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthday, presence: true
 end
--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -1,2 +1,4 @@
 class Workplace < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
 end
