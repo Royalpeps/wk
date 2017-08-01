5c46d0eb93b664c654c7d226207d395a370dba2d
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-08-01 13:48:51 +0200

    Fixed admin interface

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ApplicationRecord
   belongs_to :workplace
   has_one :weekly_schedule, dependent: :destroy
   accepts_nested_attributes_for :weekly_schedule
+
+  rails_admin do
+      edit do
+        field :status, :enum do
+          enum do
+            [['Acceptée'],['Refusée']]
+          end
+          end
+        end
+      end
 end
