7ca92bc7f300a4d8036272bb4372ce9089fa2be3
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-08-02 17:02:21 +0200

    Fixed meta_tags

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -2,7 +2,7 @@
 
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+    content_for?(:title) ? content_for(:title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
