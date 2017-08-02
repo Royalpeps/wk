627fee033579410a661d32c997172c5b97838293
Vincent Henry-Roger <v.henryroger@gmail.com>
2017-08-02 12:43:11 +0200

    devise-off

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module WorkplaceAndKids
       generate.assets false
       generate.helper false
     end
-config.i18n.default_locale = :fr
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
