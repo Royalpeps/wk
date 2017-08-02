ff172d80ba3b9e38c84e665975e2255bebe460c9
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-08-02 15:41:10 +0200

    Fixed starting_day bug and add cards for events

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :reservations, only: [:new, :create, :edit, :update, :show]
+
   patch '/reservations/:id/confirm', to: 'reservations#confirm', as: 'confirm'
+
   resources :client_data, only: [:new, :create, :edit, :update]
+
+  resources :events, only: [:index, :show]
 
   namespace :account, as: '' do
     resource :dashboard, only: [:show], as: 'dashboard'
