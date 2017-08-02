4a6cc92749341ad378bc8e5785a83ce35cffb241
Perouze Thomas <thomasperouze@gmail.com>
2017-08-02 15:39:24 +0200

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,12 @@ class ApplicationController < ActionController::Base
   # For additional fields in app/views/devise/registrations/new.html.erb
   devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_pic, :profile_pic_cache])
 
+  # app/controllers/application_controller.rb
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   # For additional in app/views/devise/registrations/edit.html.erb
   # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :phone_number, :profession, :beneficiary_number, :insurance_name, :insurance_address, :insurance_policy, :admin])
 end
