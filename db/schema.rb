fda30b6521e6b77e49cd425ad69e72bc842379e2
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-08-03 14:14:11 +0200

    Add events in planning, changed data type for birthday in kids

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803103328) do
+ActiveRecord::Schema.define(version: 20170803115400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20170803103328) do
   create_table "kids", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "birthday"
+    t.date     "birthday"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
